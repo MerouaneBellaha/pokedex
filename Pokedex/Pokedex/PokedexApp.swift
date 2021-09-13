6e4ede2f595acfc7b7f7c1ecb9f7bce9a0fca35e
Merouane B <merouane@stamp.yt>
2021-09-13 08:53:13 +0200

    [ADD] [LoginView] - UI

---
+++ Pokedex/Pokedex/PokedexApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokedexApp.swift
+//  Pokedex
+//
+//  Created by Merouane Bellaha on 13/09/2021.
+//
+
+import SwiftUI
+
+@main
+struct PokedexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            LoginView()
+        }
+    }
+}
